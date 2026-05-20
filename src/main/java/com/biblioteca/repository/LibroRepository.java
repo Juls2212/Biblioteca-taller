81783caee31c769d2f549af48f8ff08935f678b3
Juls <juliana.penafiel@campusucc.edu.co>
2026-05-19 23:04:29 -0500

    Libros configuration complete and conection whit data base

--- src/main/java/com/biblioteca/repository/LibroRepository.java
+++ src/main/java/com/biblioteca/repository/LibroRepository.java
@@ -1,4 +1,10 @@
 package com.biblioteca.repository;
 
-public class LibroRepository {
+import com.biblioteca.model.Libro;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LibroRepository extends MongoRepository<Libro, String> {
+
 }
